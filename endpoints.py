7843e59315fddd4c6a4b3ba3f4139f188d13bf56
George Farcasiu <farcasiu.george@gmail.com>
2016-07-16 07:48:12 -0700

    Added sms handler endpoint

--- endpoints.py
+++ endpoints.py
@@ -11,5 +11,40 @@ def hello_monkey():
     resp.message("Hello, Mobile Monkey")
     return str(resp)
 
+@app.route('/sms/handle_sms', methods=["POST"])
+def handle_sms():
+    customer_phone_number = request.values["From"]
+    text_message_body = request.values["Body"]
+
+    # Check to see if the message was a HELP message
+    if re.match("^\s*HELP\s*$", text_message_body, flags=re.IGNORECASE) is not None:
+        sms.send_msg(body=config.HELP_MESSAGE_1, to=customer_phone_number)
+        sms.send_msg(body=config.HELP_MESSAGE_2, to=customer_phone_number)
+        return jsonpickle.encode({"result": 0})
+
+    # Send the customer a confirmation message if its the first message
+    if len(transaction[TFields.MESSAGES]) == 1:
+        sms.send_msg(body=config.CONFIRMATION_MESSAGE, to=customer_phone_number)
+
+    return jsonpickle.encode({"result": 0})
+
+# HELPER CLASSES #
+
+sms = TwillioService('ACb5440a719947d5edf7d760155a39a768', 'dd9b4240a96556da1abb1e49646c73f3')
+
+class TwilioService(SmsService)
+    def __init__(self, account_sid, auth_token):
+        self.twilio = TwilioRestClient(account_sid, auth_token)
+
+    def is_connected(self):
+        return len(self.twilio.accounts.list()) > 0
+
+    def send_msg(self, to, body, from_="+18554164150"):
+        sms_chunks = [body[i : i + 1600]
+                for i in range(0, len(body), 1600)];
+
+        for msg in sms_chunks:
+            self.twilio.messages.create(body=msg, to=to, from_=from_)
+
 if __name__ == "__main__":
     app.run(debug=True)
